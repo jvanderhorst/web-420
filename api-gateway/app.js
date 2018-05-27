1a31d1d695bb6b78bcc0a608a1466baf0822404c
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-05-27 16:23:57 -0400

    Adding week 2 assignment

---
+++ api-gateway/app.js
@@ -0,0 +1,54 @@
+//var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+var indexRouter = require('./routes/index');
+var apiCatalog = require('./routes/api-catalog');
+
+
+var app = express();
+
+mongoose.connect('mongodb://admin:admin@ds229690.mlab.com:29690/api-gateway', {
+    promiseLibrary: require('bluebird')
+}).then ( () => console.log('connection successful'))
+  .catch( (err) => console.error(err));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiCatalog);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
