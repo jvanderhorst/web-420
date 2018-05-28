24c98512e109a70155580d1449d8cf5d1c716dbe
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-05-27 22:47:41 -0400

    Adding week 4 assignments

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -6,6 +6,16 @@ var userSchema = new mongoose.Schema({ username: String,
 password: String, email: String
 });
 module.exports = mongoose.model('User', userSchema);
+
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+};
+
 /**
 Database queries
 */
