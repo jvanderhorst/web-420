c64d95633424efe177865175383dab3077afbd8a
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-06-24 14:18:02 -0400

    Adding week 8 assignments

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 
 var auth_controller = require('../controllers/authController');
@@ -11,7 +12,7 @@ var auth_controller = require('../controllers/authController');
 router.post('/auth/register', auth_controller.user_register);
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 // POST request for signing users in
 router.post('/auth/login', auth_controller.user_login);
